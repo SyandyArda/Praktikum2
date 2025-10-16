474a694bdf8f76efac3cba7a5463727fba847550
SyandyArda <syandi.2901@gmail.com>
2025-10-16 16:35:07 +0700

    memanggil compos layout utama dengan padding dari Scaffold

--- app/src/main/java/com/example/praktikum2/MainActivity.kt
+++ app/src/main/java/com/example/praktikum2/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Praktikum2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+
+                        TataletakBoxColumnRow(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
